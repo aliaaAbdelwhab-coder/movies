7bce581c7cd7a6f15e7aedecaa2a093f218611df
MinaWagdy228 <minawagdy2228@gmail.com>
2025-01-29 16:56:33 +0200

    added tabs folder, in which there are folders for each screen by its name, did the basic ui of the application (i.e the nav bar), now the app is ready for each screen to be added in its file.
    PS: Ordering and Naming of the folders and files newly added need checking and/or readjusting

---
+++ lib/ui/tabs/browse/browse_tab.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BrowseTab extends StatelessWidget {
+  static const String routeName = 'browse_screen';
+
+  const BrowseTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.pink,width: 200, height: 200,); // just testing to see if the tabs are properly displayed
+  }
+}
