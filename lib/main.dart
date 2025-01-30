9a905692ffaf9bc10b1c4e2601ae79bb148a4f90
aliaaAbdelwhab-coder <116451688+aliaaAbdelwhab-coder@users.noreply.github.com>
2025-01-31 01:57:40 +0200

    Revert "Development"

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,6 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:movies/Register/registerUI.dart';
 import 'package:movies/home.dart';
-import 'package:movies/ui/tabs/browse/browse_tab.dart';
-import 'package:movies/ui/forgot_password/forgot_password_screen.dart';
-import 'package:movies/ui/tabs/home/home_tab.dart';
-import 'package:movies/ui/login/login%20screen.dart';
-import 'package:movies/ui/tabs/profile/profile_tab.dart';
-import 'package:movies/ui/tabs/search/search_tab.dart';
-
-import 'package:movies/utils/app_theme.dart';
 
 void main() async {
   runApp(MyApp());
@@ -20,20 +11,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Registerui(),
+      initialRoute: Home.homeRoute,
       routes: {
         Home.homeRoute: (context) => Home(),
-        Registerui.registerRoute  :(context)=>Registerui(),
-        LoginScreen.routeName: (context) => LoginScreen(),
-        ForgotPasswordScreen.routeName: (context) => ForgotPasswordScreen(),
-        HomeTab.routeName: (context) => HomeTab(),
-        ProfileTab.routeName: (context) => ProfileTab(),
-        SearchTab.routeName: (context) => SearchTab(),
-        BrowseTab.routeName: (context) => BrowseTab(),
-
       },
-      theme: AppTheme.myTheme,
     );
-    
   }
 }
