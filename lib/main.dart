2d276e9f4133fa7a4f751bf2fc11d01607928493
Amira <amiraelmezien@gmail.com>
2025-01-31 08:39:51 +0200

    Add On Boarding screen to the project.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:movies/home.dart';
+import 'package:movies/utils/app_theme.dart';
+
+import 'on_boarding_screens/Introduction_screen.dart';
+import 'on_boarding_screens/on_boarding_screen.dart';
 
 void main() async {
   runApp(MyApp());
@@ -11,8 +14,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: Home.homeRoute,
+      theme: AppTheme.myTheme,
+      initialRoute: IntroductionScreen.routeName,
       routes: {
+        IntroductionScreen.routeName: (context) => IntroductionScreen(),
+        OnBoardingScreen.routeName: (context) => OnBoardingScreen(),
         Home.homeRoute: (context) => Home(),
       },
     );
---
+++ lib/on_boarding_screens/Introduction_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import '../../utils/assets_manager.dart';
+import '../utils/app_colors.dart';
+import '../utils/app_styles.dart';
+import 'on_boarding_screen.dart';
+
+class IntroductionScreen extends StatelessWidget {
+  static const String routeName = 'IntroductionScreen';
+
+  @override
+  Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+        body: Stack(alignment: Alignment.center, children: [
+      Image.asset(
+        AssetsManager.onBoarding1,
+        width: double.infinity,
+        fit: BoxFit.cover,
+      ),
+      Spacer(),
+      Positioned(
+        bottom: height * .206,
+        // left: width*.032,
+        // right: width*.03,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: width * .05),
+          child: Text(
+            '    Find Your Next \nFavorite Movie Here',
+            style: AppStyles.medium36WhiteInter,
+          ),
+        ),
+      ),
+      SizedBox(
+        height: height * .02,
+      ),
+      Positioned(
+        bottom: height * .1,
+        left: width * .07,
+        right: width * .07,
+        child: Text(
+          '\nGet access to a huge library of movies '
+          'to suit all tastes. You will surely like it.',
+          style: AppStyles.regular20WhiteInter,
+        ),
+      ),
+      Spacer(),
+      Positioned(
+        width: width * .9,
+        bottom: height * .015,
+        child: ElevatedButton(
+            onPressed: () {
+              Navigator.of(context)
+                  .pushReplacementNamed(OnBoardingScreen.routeName);
+            },
+            style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(
+                    horizontal: width * .04, vertical: height * .016),
+                backgroundColor: AppColors.yellowColor,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16))),
+            child: Text(
+              'Explore',
+              style: AppStyles.semiBold20BlackInter,
+            )),
+      ),
+    ]));
+  }
+}
