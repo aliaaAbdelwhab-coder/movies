f5f1bbe594ef14e312b7e1839eae6bec6648d507
hanakhaled167 <khaledhana513@gmail.com>
2025-01-31 21:43:52 +0200

    create buttom sheet

--- lib/ui/tabs/profile/update_profile.dart
+++ lib/ui/tabs/profile/update_profile.dart
@@ -2,16 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:movies/utils/app_styles.dart';
 import 'package:movies/utils/assets_manager.dart';
 import 'package:movies/widget/button%20widget.dart';
-import 'package:movies/widget/text%20field%20widget.dart'; 
+import 'package:movies/widget/text%20field%20widget.dart';
 
-class UpdateProfile extends StatelessWidget {
+class UpdateProfile extends StatefulWidget {
   static const String routeName = 'update_profile';
   const UpdateProfile({super.key});
 
   @override
+  _UpdateProfileState createState() => _UpdateProfileState();
+}
+
+class _UpdateProfileState extends State<UpdateProfile> {
+  int selectedAvatarIndex = -1;
+
+  @override
   Widget build(BuildContext context) {
-    var height = MediaQuery.sizeOf(context).height;
-    var width = MediaQuery.sizeOf(context).width;
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+    List<String> avatars = [
+      AssetsManager.Avatara0,
+      AssetsManager.Avatar1,
+      AssetsManager.Avatar2,
+      AssetsManager.Avatar3,
+      AssetsManager.Avatar4,
+      AssetsManager.Avatar5,
+      AssetsManager.Avatar6,
+      AssetsManager.Avatar7,
+      AssetsManager.Avatar8,
+    ];
 
     return Scaffold(
       backgroundColor: Colors.black,
@@ -37,65 +55,111 @@ class UpdateProfile extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Center( 
-            child: Container(
-              padding: EdgeInsets.symmetric(vertical: height * 0.06),
+          Center(
+            child: GestureDetector(
+              onTap: () {
+                showModalBottomSheet(
+                  context: context,
+                  isScrollControlled: true,
+                  backgroundColor: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+                  ),
+                  builder: (context) {
+                    return Container(
+                      padding: EdgeInsets.all(10),
+                      height: height * 0.6,
+                      child: GridView.builder(
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          crossAxisSpacing: 10,
+                          mainAxisSpacing: 10,
+                        ),
+                        itemCount: avatars.length,
+                        itemBuilder: (context, index) {
+                          bool isSelected = selectedAvatarIndex == index;
+                          return GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                selectedAvatarIndex = index;
+                              });
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                border: Border.all(
+                                  color: Colors.yellow,
+                                  width: 3,
+                                ),
+                                color: isSelected ? Colors.yellow : Colors.transparent,
+                              ),
+                              padding: EdgeInsets.all(5),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(50),
+                                child: Image.asset(avatars[index]),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  },
+                );
+              },
               child: CircleAvatar(
                 radius: 75,
                 backgroundImage: AssetImage(AssetsManager.profileAvatar),
               ),
             ),
           ),
+          SizedBox(height: height * 0.06),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10), 
+            padding: EdgeInsets.symmetric(horizontal: 10),
             child: CustomTextField(
               hintText: "John Safwat",
-              prefixIcon: Icon(Icons.person, color: Colors.white), 
+              prefixIcon: Icon(Icons.person, color: Colors.white),
             ),
           ),
-          SizedBox(height: 15), 
+          SizedBox(height: 15),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10), 
+            padding: EdgeInsets.symmetric(horizontal: 10),
             child: CustomTextField(
               hintText: "0120000000",
               keyboard: TextInputType.phone,
-              prefixIcon: Icon(Icons.phone, color: Colors.white), 
+              prefixIcon: Icon(Icons.phone, color: Colors.white),
             ),
           ),
-          SizedBox(height: 25), 
+          SizedBox(height: 25),
           Padding(
-            padding: EdgeInsets.only(left: 15), 
+            padding: EdgeInsets.only(left: 15),
             child: Text(
               "Reset Password",
               style: AppStyles.regular15WhiteRoboto,
             ),
           ),
-          SizedBox(height: 70), 
+          SizedBox(height: 120),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 20),
             child: Column(
               children: [
                 SizedBox(
-                  width: double.infinity, 
+                  width: double.infinity,
                   child: CustomElevatedButton(
                     text: "Delete Account",
                     backgroundColor: Colors.red,
                     borderColor: Colors.red,
                     textStyle: AppStyles.regular20WhiteRoboto,
-                    onButtonClicked: () {
-                    },
+                    onButtonClicked: () {},
                   ),
                 ),
                 SizedBox(height: 20),
                 SizedBox(
-                  width: double.infinity, 
+                  width: double.infinity,
                   child: CustomElevatedButton(
                     text: "Update Data",
                     backgroundColor: Colors.amber,
                     borderColor: Colors.yellow,
-                    onButtonClicked: () {
-                     
-                    },
+                    onButtonClicked: () {},
                   ),
                 ),
               ],
@@ -106,5 +170,6 @@ class UpdateProfile extends StatelessWidget {
     );
   }
 }
+
 
 
