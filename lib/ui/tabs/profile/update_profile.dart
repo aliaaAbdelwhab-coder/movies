28633fe3f0d0ceabb119208e358b71e45e29d3ea
aliaaAbdelwhab-coder <aliaaabdelwhab127@gmail.com>
2025-01-31 21:34:15 +0200

    navigation and update profile

--- lib/ui/tabs/profile/update_profile.dart
+++ lib/ui/tabs/profile/update_profile.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:movies/utils/app_styles.dart';
 import 'package:movies/utils/assets_manager.dart';
 import 'package:movies/widget/button%20widget.dart';
-import 'package:movies/widget/text%20field%20widget.dart'; 
+import 'package:movies/widget/text%20field%20widget.dart';
 
 class UpdateProfile extends StatelessWidget {
   static const String routeName = 'update_profile';
@@ -17,27 +17,34 @@ class UpdateProfile extends StatelessWidget {
       backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            IconButton(
-              icon: Icon(Icons.arrow_back, color: Colors.amber, size: 25),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-            SizedBox(width: 150),
-            Text(
-              'Pick Avatar',
-              style: AppStyles.regular16YellowRoboto,
-            ),
-          ],
+        title: Text(
+          'Pick Avatar',
+          style: AppStyles.regular16YellowRoboto,
         ),
+        centerTitle: true,
+        //  Row(
+        //   mainAxisAlignment: MainAxisAlignment.start,
+        //   children: [
+        //     IconButton(
+        //       icon: Icon(Icons.arrow_back, color: Colors.amber, size: 25),
+        //       onPressed: () {
+        //         Navigator.pop(context);
+        //       },
+        //     ),
+        //     SizedBox(width: 150),
+        //     Text(
+        //       'Pick Avatar',
+        //       style: AppStyles.regular16YellowRoboto,
+        //     ),
+
+        //   ],
+        // ),
       ),
-      body: Column(
+      body:
+       Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Center( 
+          Center(
             child: Container(
               padding: EdgeInsets.symmetric(vertical: height * 0.06),
               child: CircleAvatar(
@@ -47,55 +54,52 @@ class UpdateProfile extends StatelessWidget {
             ),
           ),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10), 
+            padding: EdgeInsets.symmetric(horizontal: 10),
             child: CustomTextField(
               hintText: "John Safwat",
-              prefixIcon: Icon(Icons.person, color: Colors.white), 
+              prefixIcon: Icon(Icons.person, color: Colors.white),
             ),
           ),
-          SizedBox(height: 15), 
+          SizedBox(height: 15),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10), 
+            padding: EdgeInsets.symmetric(horizontal: 10),
             child: CustomTextField(
               hintText: "0120000000",
               keyboard: TextInputType.phone,
-              prefixIcon: Icon(Icons.phone, color: Colors.white), 
+              prefixIcon: Icon(Icons.phone, color: Colors.white),
             ),
           ),
-          SizedBox(height: 25), 
+          SizedBox(height: 25),
           Padding(
-            padding: EdgeInsets.only(left: 15), 
+            padding: EdgeInsets.only(left: 15),
             child: Text(
               "Reset Password",
               style: AppStyles.regular15WhiteRoboto,
             ),
           ),
-          SizedBox(height: 70), 
+          SizedBox(height:height*0.24),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 20),
             child: Column(
               children: [
                 SizedBox(
-                  width: double.infinity, 
+                  width: double.infinity,
                   child: CustomElevatedButton(
                     text: "Delete Account",
                     backgroundColor: Colors.red,
                     borderColor: Colors.red,
                     textStyle: AppStyles.regular20WhiteRoboto,
-                    onButtonClicked: () {
-                    },
+                    onButtonClicked: () {},
                   ),
                 ),
                 SizedBox(height: 20),
                 SizedBox(
-                  width: double.infinity, 
+                  width: double.infinity,
                   child: CustomElevatedButton(
                     text: "Update Data",
                     backgroundColor: Colors.amber,
                     borderColor: Colors.yellow,
-                    onButtonClicked: () {
-                     
-                    },
+                    onButtonClicked: () {},
                   ),
                 ),
               ],
@@ -106,5 +110,3 @@ class UpdateProfile extends StatelessWidget {
     );
   }
 }
-
-
