e26c05e7cf2abf5596928e060527c2b49b8d3667
aliaaAbdelwhab-coder <aliaaabdelwhab127@gmail.com>
2025-02-14 07:40:06 +0200

    add genres

--- lib/ui/tabs/home/movei_details/movieDetailsGenres.dart
+++ lib/ui/tabs/home/movei_details/movieDetailsGenres.dart
@@ -1,13 +1,36 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:movies/utils/app_colors.dart';
+import 'package:movies/utils/app_styles.dart';
 
-// class Moviedetailsgenres extends StatelessWidget {
-//   const Moviedetailsgenres({super.key});
+class Moviedetailsgenres extends StatelessWidget {
+  List<String> genres;
+  Moviedetailsgenres({required this.genres});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Expanded(
-//       child: GridView(gridDelegate: null,
-//         ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    
+    return Wrap(
+      spacing:size.width*0.02  , // Horizontal space between items
+      runSpacing: size.height*0.02, // Vertical space between lines
+      children: genres.map((genre) {
+        return Container(
+          width: size.width*0.3,
+          height: size.height*0.05,
+          child: Center(
+            child: Text(
+              genre,
+              style: AppStyles.regular16WhiteRoboto,
+            ),
+          ),
+          decoration: BoxDecoration(
+            color: AppColors.darkGreyColor,
+            borderRadius: BorderRadius.circular(12)
+          ),
+        
+          
+        );
+      }).toList(),
+    );
+  }
+}
