ab0c41bdb4d60d77b65d6b51ef0b6c2ed36ab923
aliaaAbdelwhab-coder <116451688+aliaaAbdelwhab-coder@users.noreply.github.com>
2025-02-18 12:36:17 +0200

    Revert "brows tab get movies and  handle images error"

--- lib/ui/tabs/home/home_tab/movie_item.dart
+++ lib/ui/tabs/home/home_tab/movie_item.dart
@@ -60,30 +60,14 @@ class MovieItem extends StatelessWidget {
                       fit: StackFit.expand,
                       children: [
                         CachedNetworkImage(
-                       imageUrl: movie.largeCoverImage?.isNotEmpty == true
-                              ? movie.largeCoverImage!
-                              : 'https://via.placeholder.com/70', // Fallback placeholder
-                          // width: width *0.1,
-                          // height: height *.1,
+                          imageUrl: movie.largeCoverImage ?? '',
                           fit: BoxFit.cover,
-
                           placeholder: (context, url) => Center(
                               child: CircularProgressIndicator(
                                 color: AppColors.darkGreyColor,
                               )),
-
                           errorWidget: (context, url, error) =>
-                              Icon(Icons.error, color: Colors.red),
-
-                          //================================================
-                          // imageUrl: movie.largeCoverImage ?? '',
-                          // fit: BoxFit.cover,
-                          // placeholder: (context, url) => Center(
-                          //     child: CircularProgressIndicator(
-                          //   color: AppColors.darkGreyColor,
-                          // )),
-                          // errorWidget: (context, url, error) =>
-                          //     Icon(Icons.error),
+                              Icon(Icons.error),
                         ),
                         Positioned(
                           top: 16,
